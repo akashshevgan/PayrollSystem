3cc28c4149968419debf700f48644266c7438d04
akash <akash@MSI>
2021-05-29 00:07:01 +0530

    Creating database Connection

--- src/main/java/DatabaseCon.java
+++ src/main/java/DatabaseCon.java
@@ -1,5 +1,29 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class DatabaseCon {
-    public static void main(String args[]){
-        System.out.println("wellcome Employee payroll Program");
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+        String userName = "root";
+        String password = "root";
+        Connection conn = null;
+        try {
+            System.out.println("Connecting to database:"+jdbcURL);
+            conn = DriverManager.getConnection(jdbcURL,userName,password);
+            System.out.println("Connection is Successful"+conn);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers(){
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass =  (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
     }
 }
